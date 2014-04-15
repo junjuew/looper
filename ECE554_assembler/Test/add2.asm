f4af3ff19207105b92e42216872cc12020f89735
oozliuoo <oozliuoo@gmail.com>
2014-04-15 16:58:57 -0500

    Modified tests for add

--- ECE554_assembler/Test/add2.asm
+++ ECE554_assembler/Test/add2.asm
@@ -3,5 +3,3 @@
 ldi r1, -18      // load -18 to r1
 ldi r2, 20      // load 20 to r2
 add r3, r1, r2  // expect r3 = 2
-ldi r1, 0
-jr  r1, 0       // jump to memory 0
--- ECE554_assembler/Test/add3.asm
+++ ECE554_assembler/Test/add3.asm
@@ -3,4 +3,3 @@
 ldi r1, 0     // load -0 to r1
 ldi r2, 15      // load 15 to r2
 add r3, r1, r2  // expect r3 = 15
-jr  r1, 0       // jump to memory 0
