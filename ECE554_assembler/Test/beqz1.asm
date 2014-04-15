43d28e28e66ba378ec385915ecf15f04cf211ae9
oozliuoo <oozliuoo@gmail.com>
2014-04-15 16:40:55 -0500

    Added branch tests

---
+++ ECE554_assembler/Test/beqz1.asm
@@ -0,0 +1,26 @@
+// beqz test, test if register is negative , zero and positive
+// and test positive branch offset
+ldi r1, 1            // set r1 to 1
+ldi r2, 2	    	 // set r2 to 2
+ldi r4, 4			 // set r4 to 4
+sub r3, r1, r2       // r3 = r1 - r2 = -1
+beqz r3, .label1     // it will not do branch here
+add r3, r3, r2       // r3 = r3 + r2 = 1
+beqz r3, .label1     // it will not do branch here
+sub r3, r3, r1       // set r3 = r3 - r1 = 0
+beqz r3, .label1     // it will do branch here
+add r3, r1, r2		 // set r3 = -1, but this line will be skipped
+
+.label1:
+ldi r1, 2            // set r1 to 2
+ldi r2, 2	     	 // set r2 to 1
+sub r3, r1, r2		 // r3 = 0
+beqz r3, .label2     // it will do branch here
+add r4, r4, r4	     // if the beqz above didn't branch, r4 will be 8
+ldi r1, 0
+jr r1, 0
+
+.label2:
+add r4, r4, r1       //if the beqz above brance, r4 will be 6
+ldi r1, 0
+jr r1, 0
---
+++ ECE554_assembler/Test/bltz1.asm
@@ -0,0 +1,26 @@
+// bltz test, test if register is negative , zero and positive
+// and test positive branch offset
+ldi r1, 1            // set r1 to 1
+ldi r2, 1	    	 // set r2 to 2
+ldi r4, 2			 // set r4 to 4
+add r3, r1, r2       // r3 = r1 + r2 = 2
+bltz r3, .label1     // it will not do branch here
+sub r3, r3, r4       // r3 = r3 - r4 = 0
+bltz r3, .label1     // it will not do branch here
+sub r3, r3, r1       // set r3 = r3 - r1 = -1
+bltz r3, .label1     // it will do branch here
+add r3, r1, r2		 // set r3 = 2, but this line will be skipped
+
+.label1:
+ldi r1, 2            // set r1 to 2
+ldi r2, 2	     	 // set r2 to 2
+sub r3, r1, r2		 // r3 = 0
+beqz r3, .label2     // it will do branch here
+add r4, r4, r4	     // if the beqz above didn't branch, r4 will be 4
+ldi r1, 0
+jr r1, 0
+
+.label2:
+add r4, r4, r1       //if the beqz above brance, r4 will be 3
+ldi r1, 0
+jr r1, 0
