26a4435eeb6a9eb03d244d7503b06f62327bf4af
oozliuoo <oozliuoo@gmail.com>
2014-04-22 15:33:29 -0500

    Fixed typo in some tests

--- ECE554_assembler/Test/add_dep2.asm
+++ ECE554_assembler/Test/add_dep2.asm
@@ -5,4 +5,4 @@ ldi r1, -18     // load -18 to r1
 ldi r2, 20      // load 20 to r2
 add r3, r1, r2  // expect r3 = 2
 add r2, r1, r3  // expect r2 = -16
-add r4, r3, r2  // expect r4 = -4
+add r4, r3, r2  // expect r4 = -14
--- ECE554_assembler/Test/sub_dep2.asm
+++ ECE554_assembler/Test/sub_dep2.asm
@@ -3,6 +3,6 @@
 
 ldi r1, -5
 ldi r2, -10
-sub r3, r2, r1  // expect r3 = -15
-sub r4, r2, r3  // expect r4 = 5
-sub r5, r3, r4  // expect r5 = -20
+sub r3, r2, r1  // expect r3 = -5
+sub r4, r2, r3  // expect r4 = -5
+sub r5, r3, r4  // expect r5 = 0
