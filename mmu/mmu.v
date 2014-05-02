66eba5d9705267d36b498452dfd878bd24257c1e
J.J. Wang <canjian.myself@gmail.com>
2014-05-01 22:21:36 -0500

    dump output current architecture header out to file

--- mmu/mmu.v
+++ mmu/mmu.v
@@ -23,7 +23,7 @@
 
 module mmu(/*autoarg*/
    // Outputs
-   txd, enb, web, addrb, dinb, flsh,state,
+   txd, state, enb, web, addrb, dinb, flsh,
    // Inputs
    clk, rst_n, rxd, br_cfg, doutb, cpu_pc, mem_sys_fin
    );
@@ -81,6 +81,7 @@ module mmu(/*autoarg*/
                   .addrb                (addrb[13:0]),
                   .dinb                 (dinb[63:0]),
                   .flsh                 (flsh),
+                  .state                (state[3:0]),
                   // Inouts
                   .databus              (databus[7:0]),
                   // Inputs
@@ -91,8 +92,31 @@ module mmu(/*autoarg*/
                   .tbr                  (tbr),
                   .doutb                (doutb[63:0]),
                   .cpu_pc               (cpu_pc[15:0]),
-                  .mem_sys_fin          (mem_sys_fin),
-                                                .state (state));
+                  .mem_sys_fin          (mem_sys_fin));
 
+
+
+   dvi dvi0(/*autoinst*/
+            // Outputs
+            .D                          (D[11:0]),
+            .blank                      (blank),
+            .hsync                      (hsync),
+            .vsync                      (vsync),
+            .clk                        (clk),
+            .clk_n                      (clk_n),
+            .dvi_rst                    (dvi_rst),
+            .GPIO_LED_0                 (GPIO_LED_0),
+            .GPIO_LED_1                 (GPIO_LED_1),
+            .GPIO_LED_2                 (GPIO_LED_2),
+            .GPIO_LED_4                 (GPIO_LED_4),
+            .GPIO_LED_7                 (GPIO_LED_7),
+            // Inouts
+            .scl_tri                    (scl_tri),
+            .sda_tri                    (sda_tri),
+            // Inputs
+            .CLK_100MHZ                 (CLK_100MHZ),
+            .RST                        (RST));
+   
+   
    
 endmodule
